15c40175bcb32cd4ac08881bc7c263e85782e217
Cort Rockyou <cort.oi@rockyou.com>
2012-10-16 13:03:47 -0700

    Tagging for release v2.007

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, 'ruby-1.9.3-p125'        # Or whatever env you want it to run in.
 
 set :repository,  "git@github.com:hom/handson_poker.git"
-set :branch, 'v2.004'
+set :branch, 'v2.007'
 
 set :deploy_to,   "/var/www/social/wptfb/html/#{application}"
 
